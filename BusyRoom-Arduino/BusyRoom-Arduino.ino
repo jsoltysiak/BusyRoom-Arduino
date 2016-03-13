9568ea3fa38e697334fca88a1bdfa4633458f44b
Janusz Sołtysiak <jsoltysiak@gmail.com>
2016-03-13 01:03:18 +0100

    Reformatted the .ino file to have 4 space indentation

--- BusyRoom-Arduino/BusyRoom-Arduino.ino
+++ BusyRoom-Arduino/BusyRoom-Arduino.ino
@@ -20,71 +20,71 @@ byte session;
 
 
 void setup() {
-  Serial.begin(57600);
-  Serial.println("\n[webClient]");
+    Serial.begin(57600);
+    Serial.println("\n[webClient]");
 
-  if (ether.begin(sizeof Ethernet::buffer, mymac) == 0)
-    Serial.println("Failed to access Ethernet controller");
-  if (!ether.dhcpSetup())
-    Serial.println("DHCP failed");
+    if (ether.begin(sizeof Ethernet::buffer, mymac) == 0)
+        Serial.println("Failed to access Ethernet controller");
+    if (!ether.dhcpSetup())
+        Serial.println("DHCP failed");
 
-  ether.printIp("IP:  ", ether.myip);
-  ether.printIp("GW:  ", ether.gwip);
-  ether.printIp("DNS: ", ether.dnsip);
+    ether.printIp("IP:  ", ether.myip);
+    ether.printIp("GW:  ", ether.gwip);
+    ether.printIp("DNS: ", ether.dnsip);
 
-  ether.hisip[0] = 192;
-  ether.hisip[1] = 168;
-  ether.hisip[2] = 0;
-  ether.hisip[3] = 14;
+    ether.hisip[0] = 192;
+    ether.hisip[1] = 168;
+    ether.hisip[2] = 0;
+    ether.hisip[3] = 14;
 
-  ether.hisport = 8765;
+    ether.hisport = 8765;
 
-  //if (!ether.dnsLookup("google.com"))
-  // Serial.println("DNS failed");
+    //if (!ether.dnsLookup("google.com"))
+    // Serial.println("DNS failed");
 
-  ether.printIp("SRV: ", ether.hisip);
-  //  Serial.println("hisport: %D", ether.hisport);
+    ether.printIp("SRV: ", ether.hisip);
+    //  Serial.println("hisport: %D", ether.hisport);
 
-  pinMode(pirPin, INPUT);
+    pinMode(pirPin, INPUT);
 }
 
 void loop() {
-  ether.packetLoop(ether.packetReceive());
+    ether.packetLoop(ether.packetReceive());
 
-  if (millis() > timer) {
-    timer = millis() + 10000;
+    if (millis() > timer) {
+        timer = millis() + 10000;
 
-    isOccupied = digitalRead(pirPin);
+        isOccupied = digitalRead(pirPin);
 
-    byte sd = stash.create();
-    stash.print("{\"isOccupied\":\"");
-    stash.print(isOccupied ? "true" : "false");
-    stash.print("\"}");
-    stash.save();
+        byte sd = stash.create();
+        stash.print("{\"isOccupied\":\"");
+        stash.print(isOccupied ? "true" : "false");
+        stash.print("\"}");
+        stash.save();
 
-    // generate the header with payload - note that the stash size is used,
-    // and that a "stash descriptor" is passed in as argument using "$H"
-    Stash::prepare(
-      PSTR(
-        "POST http://192.168.0.14:8765$F HTTP/1.1" "\r\n"
-        "Host: $F" "\r\n"
-        "Content-Length: $D" "\r\n"
-        "Content-Type: application/json" "\r\n"
-        "\r\n"
-        "$H"),
-      PSTR(PATH), website, stash.size(), sd);
+        // generate the header with payload - note that the stash size is used,
+        // and that a "stash descriptor" is passed in as argument using "$H"
+        Stash::prepare(
+            PSTR(
+                "POST http://192.168.0.14:8765$F HTTP/1.1" "\r\n"
+                "Host: $F" "\r\n"
+                "Content-Length: $D" "\r\n"
+                "Content-Type: application/json" "\r\n"
+                "\r\n"
+                "$H"),
+            PSTR(PATH), website, stash.size(), sd);
 
-    // send the packet - this also releases all stash buffers once done
-    session = ether.tcpSend();
-    Serial.println("Sent the package!");
-  }
+        // send the packet - this also releases all stash buffers once done
+        session = ether.tcpSend();
+        Serial.println("Sent the package!");
+    }
 
-  const char* reply = ether.tcpReply(session);
+    const char* reply = ether.tcpReply(session);
 
-  if (reply != 0) {
-    Serial.println("");
-    Serial.println("Reply from the server:");
-    Serial.println(reply);
-    Serial.println("");
-  }
+    if (reply != 0) {
+        Serial.println("");
+        Serial.println("Reply from the server:");
+        Serial.println(reply);
+        Serial.println("");
+    }
 }
