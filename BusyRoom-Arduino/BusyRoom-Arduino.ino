3e126ca3e2172a752916d846d264b5fdffe0ac5c
Janusz Sołtysiak <jsoltysiak@gmail.com>
2016-03-18 07:19:27 +0100

    Added temperature and humidity sensor readings and sending it through ethernet

--- BusyRoom-Arduino/BusyRoom-Arduino.ino
+++ BusyRoom-Arduino/BusyRoom-Arduino.ino
@@ -1,11 +1,13 @@
+#include <DHT.h>
 #include <EtherCard.h>
 
 const int PIN_PIR = 7;
+const int PIN_DHT = 2;
 
 // ethernet interface mac address, must be unique on the LAN
 const byte mymac[] = { 0x74, 0x69, 0x69, 0x2D, 0x38, 0x31 };
 
-const uint8_t IP[] = {192,168,0,14};
+const uint8_t IP[] = { 192,168,0,14 };
 const uint16_t PORT = 3333;
 const char ADDRESS[] PROGMEM = "192.168.0.14:3333";
 const char PATH[] PROGMEM = "api/rooms/Arduino/states";
@@ -15,6 +17,9 @@ uint32_t timer;
 Stash stash;
 byte session;
 
+DHT dht;
+float temperature;
+float humidity;
 bool pirSensorState = false;
 
 
@@ -25,6 +30,7 @@ void setup() {
     initializeEthernet();
 
     pinMode(PIN_PIR, INPUT);
+    dht.setup(2, DHT::DHT11);
 
     timer = millis() + 3000;
 }
@@ -33,10 +39,21 @@ void loop() {
     ether.packetLoop(ether.packetReceive());
 
     if (millis() > timer) {
-        timer = millis() + 10000;
+        timer = millis() + dht.getMinimumSamplingPeriod();
 
         pirSensorState = digitalRead(PIN_PIR);
-        sendSensorReadings(pirSensorState);
+
+        temperature = dht.getTemperature();
+        humidity = dht.getHumidity();
+
+        Serial.print("pirSensorState: ");
+        Serial.println(pirSensorState);
+        Serial.print("temperature: ");
+        Serial.println(temperature);
+        Serial.print("humidity: ");
+        Serial.println(humidity);
+
+        sendSensorReadings(pirSensorState, temperature, humidity);
     }
 
     getAndPrintReply();
@@ -65,12 +82,20 @@ void initializeEthernet()
     Serial.println(ether.hisport);
 }
 
-void sendSensorReadings(bool isOccupied)
+void sendSensorReadings(bool isOccupied, float temperature, float humidity)
 {
     byte sd = stash.create();
-    stash.print("{\"isOccupied\":\"");
+    stash.print("{");
+    stash.print("\"isOccupied\":\"");
     stash.print(isOccupied ? "true" : "false");
-    stash.print("\"}");
+    stash.print(",");
+    stash.print("\"temperature\":\"");
+    stash.print(temperature);
+    stash.print("\",");
+    stash.print("\"humidity\":\"");
+    stash.print(humidity);
+    stash.print("\"");
+    stash.print("}");
     stash.save();
 
     // generate the header with payload - note that the stash size is used,
